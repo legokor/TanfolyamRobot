6476c251e76745a80bf00fcc25b339e335800d01
ksstms <ksstms96@gmail.com>
2020-11-13 15:32:44 +0100

    added time measurement to upload script, increased baud rate

--- firmware/upload.sh
+++ firmware/upload.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+APPLICATION_BAUD=115200
+UPLOAD_BAUD=500000
+
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 port"
 	exit 1
 fi
 
-stty -F $1 115200
+stty -F $1 $APPLICATION_BAUD
 
 echo "Enter DFU mode..."
 echo -n ENTER_DFU > $1
+
 sleep 1
 
-stm32flash -b 115200 -w Debug/tanfolyamrobot.bin -v -g 0x0 $1
+START_TIME=$(date +%s.%N)
+
+stm32flash -b $UPLOAD_BAUD -w Debug/tanfolyamrobot.bin -v -g 0x0 $1
+
+END_TIME=$(date +%s.%N)
+DIFF=$(echo "$END_TIME - $START_TIME" | bc)
+echo "Upload time: $DIFF"
