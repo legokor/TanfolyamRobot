cd91a73a5f5674b54b0cc6aae70f6513a980ac60
Tamás Kiss <ksstms96@gmail.com>
2021-10-19 13:28:01 +0200

    refactored soft-servo to use soft-pwm (#7)

--- firmware/Drivers/servo/soft-servo.c
+++ firmware/Drivers/servo/soft-servo.c
@@ -4,14 +4,13 @@
  *  Created on: Oct 10, 2021
  *      Author: ksstms
  *
- * Software PWM generator for controlling servo motors.
+ * This driver uses the software-PWM driver to generate the control signal for servo motors.
  * Most servos need a 50 Hz PWM signal with pulse width between 1 and 2 ms.
  * 1 ms sets the position to -90 degrees, and 2 ms to +90 degrees.
  *
- * The handler function shall be called from a timer interrupt.
- * The period property shall be set to the timer interrupt frequency / 50 Hz.
- * The start and end properties shall be set to about period/20 and period/10,
- * but they might need some tweaking to work properly.
+ * The handler function shall be called from a timer interrupt. The period property shall be
+ * set to the timer interrupt frequency / 50 Hz. The start and end properties shall be set
+ * to about period/20 and period/10, but they might need some tweaking to work properly.
  */
 #include "soft-servo.h"
 
@@ -19,21 +18,47 @@
  * Initialize a servo struct
  * @note compareStart and compareEnd values can be swapped for inverse rotation
  * @param servo to be initialized
- * @param port GPIO port of the control signal
- * @param pin GPIO pin of the control signal
- * @param period of the internal counter
- * @param start counter value for one endpoint
- * @param end counter value for the other endpoint
+ * @param pwm soft PWM generating the output signal
+ * @param compareStart output compare value for -90 degree position (1 ms)
+ * @param compareEnd output compare value for +90 degree position (2 ms)
  */
-void softServoInit(SoftServo* servo, GPIO_TypeDef* port, uint16_t pin,
-		           uint32_t period, uint32_t start, uint32_t end) {
-	servo->port = port;
-	servo->pin = pin;
-	servo->cntr = 0;
-	servo->period = period;
-	servo->start = start;
-	servo->end = end;
-	servo->compareVal = (start + end) / 2;
+void softServoInit(SoftServo* servo, SoftPwm* pwm, uint32_t compareStart, uint32_t compareEnd) {
+    servo->pwm = pwm;
+    servo->compareStart = compareStart;
+    servo->compareEnd = compareEnd;
+
+    uint32_t mid = (compareStart + compareEnd) / 2;
+
+    softPwmSetCompareValue(servo->pwm, mid);
+}
+
+/**
+ * Create a Servo struct. This also creates the Pwm used for the servo control.
+ * @note This function allocates memory for the structs!
+ * @note compareStart and compareEnd values can be swapped for inverse rotation
+ *
+ * @param port GPIO port of the output signal
+ * @param pin GPIO pin of the output signal
+ * @param period of the PWM's internal counter
+ * @param compareStart output compare value for -90 degree position (1 ms)
+ * @param compareEnd output compare value for +90 degree position (2 ms)
+ * @return pointer to the created struct, NULL on error
+ */
+SoftServo* softServoCreate(GPIO_TypeDef* port, uint16_t pin, uint32_t period,
+                           uint16_t compareStart, uint16_t compareEnd) {
+
+    SoftPwm* pwm = softPwmCreate(port, pin, period);
+    if (pwm == NULL) {
+        return NULL;
+    }
+
+    SoftServo* servo = (SoftServo*) malloc(sizeof(SoftServo));
+
+    if (servo != NULL) {
+        softServoInit(servo, pwm, compareStart, compareEnd);
+    }
+
+    return servo;
 }
 
 /**
@@ -49,13 +74,15 @@ void softServoSetPosition(SoftServo* servo, int8_t position) {
         position = -90;
     }
 
-    int32_t s = servo->start;
-    int32_t e = servo->end;
+    int32_t s = servo->compareStart;
+    int32_t e = servo->compareEnd;
 
     uint16_t mid = s + (e-s)/2;
     int32_t diff = (e-mid) * position / 90;
 
-    servo->compareVal = mid + diff;
+    uint32_t compareVal = mid + diff;
+
+    softPwmSetCompareValue(servo->pwm, compareVal);
 }
 
 /**
@@ -63,15 +90,5 @@ void softServoSetPosition(SoftServo* servo, int8_t position) {
  * @param servo
  */
 void softServoHandler(SoftServo* servo) {
-	servo->cntr++;
-
-	if (servo->cntr == servo->period) {
-		servo->cntr = 0;
-	}
-
-	if (servo->cntr < servo->compareVal) {
-		HAL_GPIO_WritePin(servo->port, servo->pin, GPIO_PIN_SET);
-	} else {
-		HAL_GPIO_WritePin(servo->port, servo->pin, GPIO_PIN_RESET);
-	}
+    softPwmHandler(servo->pwm);
 }
