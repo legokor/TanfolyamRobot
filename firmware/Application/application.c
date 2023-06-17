73308ee8c13094a9d9cbc92381ac64e1af110468
Kiovics Dániel <dkiovics@gmail.com>
2023-06-17 16:50:58 +0200

    environment settings fix

--- firmware/Application/application.c
+++ firmware/Application/application.c
@@ -8,12 +8,6 @@ int application() {
     lcdPrintf(0, 0, "Hello");
     lcdPrintf(1, 5, "World!");
 
-    for(int p = 0; p < 50; p++){
-    	uartPrintf("Na csaa\n");
-    	uartPrintf("Szia ");
-    	uartPrintf("hello!\n");
-    }
-
     delayMs(2000);
 
     return 0;
