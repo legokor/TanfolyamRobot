acdcc56b0609982626a86fa373cdd434b4273b5a
Kiovics Dániel <dkiovics@gmail.com>
2023-07-04 18:12:54 +0200

    data format changed

--- firmware/Application/application.c
+++ firmware/Application/application.c
@@ -11,12 +11,12 @@ const int stageMs = 2000;
 const int stages[] = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
 const int stageNum = 20;
 
-int activeStage = 0;
+int speed = 0;
 
 //elapsed - time since last encoder tick (in 64MHz clock ticks)
 void printEncoderT(Encoder* encoder, uint32_t elapsed){
 	if((motorEncoder == MOT_L && encoder == &encoder2) || (motorEncoder == MOT_R && encoder == &encoder1))
-		uartPrintf("%d;%d\n", activeStage, elapsed);
+		uartPrintf("%d;%d\n", speed, elapsed);
 }
 
 
@@ -27,8 +27,9 @@ int application() {
 
     delayMs(1000);
 
-    for(activeStage = 0; activeStage < stageNum; activeStage++){
-    	setMotorSpeed(motorEncoder, stages[activeStage]);
+    for(int p = 0; p < stageNum; p++){
+    	speed = stages[p];
+    	setMotorSpeed(motorEncoder, speed);
     	delayMs(stageMs);
     	setMotorSpeed(motorEncoder, 0);
 		delayMs(stageMs);
