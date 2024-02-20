4cce2e30c5c316172ef07f87bb442fae2cb7a5e9
Kiovics Dániel <dkiovics@gmail.com>
2024-02-21 00:21:59 +0100

    ESP bug fixes, IMU UI deactivated

--- firmware/Application/application.c
+++ firmware/Application/application.c
@@ -3,11 +3,11 @@
  */
 #include "robotcontrol-api.h"
 
-const char* WIFI_SSID = "Andi";
-const char* WIFI_PASSWORD = "Gomboc02";
+const char* WIFI_SSID = "Tanfolyam_WiFi";
+const char* WIFI_PASSWORD = "LegoFTW1";
 
 #define MANUAL_IP FALSE
-const char* SERVER_IP = "192.168.1.239";
+const char* SERVER_IP = "192.168.186.102";
 
 int application() {
 
@@ -19,9 +19,6 @@ int application() {
     while(1){
     	char data[300];
     	if(espRead(data)){
-    		lcdPrintf(0, 0, "                ");
-    		lcdPrintf(1, 0, "                ");
-    		lcdPrintf(0, 0, data);
     		espPrintf("I received: %s", data);
     	}
     }
