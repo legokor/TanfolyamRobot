204257c907dbf3ea36fbb2effb8b29ad444a9e29
Kiovics Dániel <dkiovics@gmail.com>
2024-07-06 21:00:17 +0200

    fixup! service created

--- react-robot-dashboard/start.sh
+++ react-robot-dashboard/start.sh
@@ -4,4 +4,4 @@ echo "Starting the React Robot Dashboard"
 
 (cd /WS/Tanfrobot/TanfolyamRobot/react-robot-dashboard/my-robot-server && npm start) &
 
-(cd /WS/Tanfrobot/TanfolyamRobot/react-robot-dashboard/robot-dashboard && npm start)
+(cd /WS/Tanfrobot/TanfolyamRobot/react-robot-dashboard/robot-dashboard && serve -s build)
