f33908d50e528fd8ae722d091745bdfd0aa5143a
Kiovics Dániel <dkiovics@gmail.com>
2024-07-09 23:26:14 +0200

    Full IMU screen

--- react-robot-dashboard/robot-dashboard/src/IMUSensorDisplay.js
+++ react-robot-dashboard/robot-dashboard/src/IMUSensorDisplay.js
@@ -6,7 +6,7 @@ const IMUSensorDisplay = ({ imuData }) => {
     return <div>Loading...</div>; // or some placeholder content
   }
 
-  const { acc, gyro, mag, temp } = imuData;
+  const { acc, gyro, mag, angle, temp } = imuData;
 
   return (
     <div className="imu-sensor-display">
@@ -14,25 +14,28 @@ const IMUSensorDisplay = ({ imuData }) => {
       <div className="imu-matrix">
         <div className="imu-column">
           <div className="imu-header">Acc <br />(m/s²)</div>
-          <div className="imu-value">{acc.x}</div>
-          <div className="imu-value">{acc.y}</div>
-          <div className="imu-value">{acc.z}</div>
+          <div className="imu-value">{acc.x.toFixed(3)}</div>
+          <div className="imu-value">{acc.y.toFixed(3)}</div>
+          <div className="imu-value">{acc.z.toFixed(3)}</div>
         </div>
         <div className="imu-column">
           <div className="imu-header">Gyro (°/s)</div>
-          <div className="imu-value">{gyro.x}</div>
-          <div className="imu-value">{gyro.y}</div>
-          <div className="imu-value">{gyro.z}</div>
+          <div className="imu-value">{gyro.x.toFixed(1)}</div>
+          <div className="imu-value">{gyro.y.toFixed(1)}</div>
+          <div className="imu-value">{gyro.z.toFixed(1)}</div>
         </div>
         <div className="imu-column">
           <div className="imu-header">Mag (uT)</div>
-          <div className="imu-value">{mag.x}</div>
-          <div className="imu-value">{mag.y}</div>
-          <div className="imu-value">{mag.z}</div>
+          <div className="imu-value">{mag.x.toFixed(2)}</div>
+          <div className="imu-value">{mag.y.toFixed(2)}</div>
+          <div className="imu-value">{mag.z.toFixed(2)}</div>
         </div>
         <div className="imu-column">
           <div className="imu-header">Temperature (°C)</div>
-          <div className="imu-value">{temp}</div>
+          <div className="imu-value">{temp.toFixed(1)}</div>
+          <div className="imu-header">Angle (°)</div>
+          <div className="imu-value">{angle.pitch.toFixed(1)}</div>
+          <div className="imu-value">{angle.roll.toFixed(1)}</div>
         </div>
       </div>
     </div>
