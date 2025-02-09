1d54a0bbe62140570dba1a7d911aa7cdb724cbef
Kolos Kis-Bogdán <kisbogdan.kolos@gmail.com>
2025-02-09 23:43:29 +0100

    feat: linux suport

--- firmware/tools/ftdi_uploader/main.cpp
+++ firmware/tools/ftdi_uploader/main.cpp
@@ -18,38 +18,36 @@
 // COM port     Baud rate      BIN file path
 // OR
 // Baud rate      BIN file path          <--------- use default (first FTDI device) COM port number
-int main(int argc, char* argv[]) 
-{
-    if (argc != 4 && argc != 3)
-    {
-        std::cout << "Num of arguments: " << argc 
+int main(int argc, char *argv[]) {
+    if (argc != 4 && argc != 3) {
+        std::cout << "Num of arguments: " << argc
             << "\nError: Upload params should be: COM_PORT BAUD_RATE BIN_FILE_PATH   or   BAUD_RATE BIN_FILE_PATH\n"
             << std::endl;
         return -1;
     }
-    
+
     FT_STATUS ftStatus;
     FT_HANDLE ftHandle;
 
     std::cout << "TanfolyamRobot loader V3.0\n" << std::endl;
     std::cout << "WARNING: Make sure only one FTDI device is plugged in!\n" << std::endl;
 
+#ifndef _WIN32
+    system("sudo modprobe -r ftdi_sio");
+#endif
+
     ftStatus = FT_Open(0, &ftHandle);
-    if (ftStatus != FT_OK)
-    {
+    if (ftStatus != FT_OK) {
         std::cout << "Cannot open FT232, error code: " << ftStatus << "\n" << std::endl;
         return 1;
-    }
-    else
-    {
+    } else {
         std::cout << "Connection with FT232 successful\n" << std::endl;
     }
 
     bool success = true;
     LONG comPortNumber = 0;
 
-    try
-    {
+    try {
         ftStatus = FT_SetBitMode(ftHandle, 0xCC, 0x20);
         if (ftStatus != FT_OK)
             throw std::runtime_error("Failed to reset STM32, FTDI error code: " + std::to_string(ftStatus));
@@ -62,35 +60,26 @@ int main(int argc, char* argv[])
             throw std::runtime_error("Failed to init STM32, FTDI error code: " + std::to_string(ftStatus));
         std::cout << "STM32 is in bootloader mode\n" << std::endl;
 
-        if (argc == 3)
-        {
+        if (argc == 3) {
             ftStatus = FT_GetComPortNumber(ftHandle, &comPortNumber);
-            if (ftStatus == FT_OK)
-            {
-                if (comPortNumber == -1)
-                {
+            if (ftStatus == FT_OK) {
+                if (comPortNumber == -1) {
                     throw std::runtime_error("Failed to detect device COM port number");
-                }
-                else
-                {
+                } else {
                     std::cout << "Detected device COM port number is " << comPortNumber << "\n" << std::endl;
                 }
-            }
-            else
-            {
+            } else {
                 throw std::runtime_error("FT_GetComPortNumber failed, error code: " + std::to_string(ftStatus));
             }
         }
     }
-    catch (std::runtime_error e)
-    {
+    catch (std::runtime_error e) {
         success = false;
         std::cout << e.what() << std::endl;
     }
-    
+
     ftStatus = FT_Close(ftHandle);
-    if (ftStatus != FT_OK)
-    {
+    if (ftStatus != FT_OK) {
         std::cout << "Cannot close FT232, error code: " << ftStatus << "\n" << std::endl;
         return 1;
     }
@@ -100,15 +89,24 @@ int main(int argc, char* argv[])
 
     std::this_thread::sleep_for(std::chrono::milliseconds(400));
 
+#ifndef _WIN32
+    system("sudo modprobe ftdi_sio");
+    system("sleep 1");
+#endif
+
     std::string cmd;
-    if(argc == 4)
+    if (argc == 4)
+#ifdef _WIN32
         cmd = std::string("stm32flash") + " -b " + argv[2] + " -w " + argv[3] + " -v -g 0x0 " + argv[1];
+#else
+        cmd = std::string("./stm32flash") + " -b " + argv[2] + " -w " + argv[3] + " -v -g 0x0 " + argv[1];
+#endif
     else
-    #ifdef _WIN32
+#ifdef _WIN32
         cmd = std::string("stm32flash") + " -b " + argv[1] + " -w " + argv[2] + " -v -g 0x0 " + "COM" + std::to_string(comPortNumber);
-    #else
-        cmd = std::string("stm32flash") + " -b " + argv[1] + " -w " + argv[2] + " -v -g 0x0 " + "/dev/ttyUSB" + std::to_string(comPortNumber);
-    #endif
+#else
+        cmd = std::string("./stm32flash") + " -b " + argv[1] + " -w " + argv[2] + " -v -g 0x0 " + "/dev/ttyUSB" + std::to_string(comPortNumber);
+#endif
     system(cmd.c_str());
 
     return 0;
